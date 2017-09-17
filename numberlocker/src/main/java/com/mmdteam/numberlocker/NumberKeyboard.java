73950bcef1518c7c4b6d3e7706dbce99b64f7855
brain <weiyinouon@163.com>
2017-09-17 22:51:16 +0800

    添加图形解锁

--- numberlocker/src/main/java/com/mmdteam/numberlocker/NumberKeyboard.java
+++ numberlocker/src/main/java/com/mmdteam/numberlocker/NumberKeyboard.java
@@ -5,6 +5,8 @@ import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,13 +16,15 @@ import android.widget.TextView;
  * 密码数字键盘
  */
 
-public class NumberKeyboard extends LinearLayout {
+public class NumberKeyboard extends LinearLayout implements AdapterView.OnItemClickListener {
     public NumberKeyboard(Context context) {
         super(context);
         init();
     }
 
-    private TextView passwordOne;
+    private TextView passwordOne, passwordTwo, passwordThree, passwordFour;
+    private int currentNum = 0;
+    private int pOne, pTwo, pThree, pFour;
 
     public NumberKeyboard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -43,7 +47,84 @@ public class NumberKeyboard extends LinearLayout {
         GridView gridView = findViewById(R.id.keyboard_grid_view);
         gridView.setAdapter(new NumberAdapter(getContext()));
         passwordOne = findViewById(R.id.password_one);
-        passwordOne.setSelected(true);
+        passwordTwo = findViewById(R.id.password_two);
+        passwordThree = findViewById(R.id.password_three);
+        passwordFour = findViewById(R.id.password_four);
+        gridView.setOnItemClickListener(this);
     }
 
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        if (currentNum > 4) {
+            return;
+        }
+
+        int cacheNum = 0;
+        switch (i) {
+            case 0:
+                cacheNum = 1;
+                break;
+            case 1:
+                cacheNum = 2;
+                break;
+            case 2:
+                cacheNum = 3;
+                break;
+            case 3:
+                cacheNum = 4;
+                break;
+            case 4:
+                cacheNum = 5;
+                break;
+            case 5:
+                cacheNum = 6;
+                break;
+            case 6:
+                cacheNum = 7;
+                break;
+            case 7:
+                cacheNum = 8;
+                break;
+            case 8:
+                cacheNum = 9;
+                break;
+            case 9:
+                break;
+            case 10:
+                cacheNum = 0;
+                break;
+            case 11:
+                break;
+        }
+        currentNum++;
+        takePassword(currentNum, cacheNum);
+        if (currentNum == 4) {
+            checkPassword(pOne, pTwo, pThree, pFour);
+        }
+    }
+
+    private void takePassword(int index, int value) {
+        switch (index) {
+            case 1:
+                pOne = value;
+                passwordOne.setSelected(true);
+                break;
+            case 2:
+                pTwo = value;
+                passwordTwo.setSelected(true);
+                break;
+            case 3:
+                pThree = value;
+                passwordThree.setSelected(true);
+                break;
+            case 4:
+                pFour = value;
+                passwordFour.setSelected(true);
+                break;
+        }
+    }
+
+    private void checkPassword(int... password) {
+    }
 }
