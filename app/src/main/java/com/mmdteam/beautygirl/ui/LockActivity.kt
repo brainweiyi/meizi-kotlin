8cf94bf220ea1f715b1be93c454d8a16f5ecaf1d
brain <weiyinouon@163.com>
2017-09-17 23:25:53 +0800

    添加数字解锁

--- app/src/main/java/com/mmdteam/beautygirl/ui/LockActivity.kt
+++ app/src/main/java/com/mmdteam/beautygirl/ui/LockActivity.kt
@@ -3,7 +3,6 @@ package com.mmdteam.beautygirl.ui
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.mmdteam.beautygirl.R
-import com.mmdteam.numberlocker.NumberAdapter
 import kotlinx.android.synthetic.main.activity_number_locker.*
 
 /**
